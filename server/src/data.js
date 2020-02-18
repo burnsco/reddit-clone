6f3f48cdc816a2b65674371e3128a04efdc607c6
Corey Burns <coreymburns@gmail.com>
2020-02-17 21:14:02 -0500

    added createPost mutation, moved typeDefs into schema file

---
+++ server/src/data.js
@@ -0,0 +1,38 @@
+export default [
+  {
+    id: '1',
+    title: 'First Post!',
+    url: 'www.google.ca',
+    comments: 3,
+    category: '/r/music',
+    author: 'cburn343',
+    votes: 34
+  },
+  {
+    id: '2',
+    title: 'Second Post!',
+    url: 'www.reddit.com',
+    comments: 3,
+    category: '/r/television',
+    author: 'tom34',
+    votes: 1
+  },
+  {
+    id: '3',
+    title: 'The very Third Post!@#@',
+    url: 'yahoo.ca',
+    comments: 3,
+    category: '/r/webdev',
+    author: 'cburn343',
+    votes: 12
+  },
+  {
+    id: '4',
+    title: '4th post!DF',
+    url: 'www.google.ca',
+    comments: 3,
+    category: '/r/react',
+    author: 'abb34dfdf',
+    votes: 312
+  }
+]
--- server/src/index.js
+++ server/src/index.js
@@ -1,66 +1,11 @@
+import posts from './data'
 const { GraphQLServer } = require('graphql-yoga')
-
-const typeDefs = `
-  type Query {
-    info: String!
-    feed: [Post!]!
-  }
-
-  type Post {
-    id: ID!
-    title: String!
-    url: String!
-    comments: Int!
-    category: String!
-    author: String!
-    votes: Int!
-  }
-`
-let posts = [
-  {
-    id: '1',
-    title: 'First Post!',
-    url: 'www.google.ca',
-    comments: 3,
-    category: '/r/music',
-    author: 'cburn343',
-    votes: 34
-  },
-  {
-    id: '2',
-    title: 'Second Post!',
-    url: 'www.reddit.com',
-    comments: 3,
-    category: '/r/television',
-    author: 'tom34',
-    votes: 1
-  },
-  {
-    id: '3',
-    title: 'The very Third Post!@#@',
-    url: 'yahoo.ca',
-    comments: 3,
-    category: '/r/webdev',
-    author: 'cburn343',
-    votes: 12
-  },
-  {
-    id: '4',
-    title: '4th post!DF',
-    url: 'www.google.ca',
-    comments: 3,
-    category: '/r/react',
-    author: 'abb34dfdf',
-    votes: 312
-  }
-]
 
 const resolvers = {
   Query: {
     info: () => `This is the API of a Reddit Clone`,
     feed: () => posts
   },
-
   Post: {
     id: parent => parent.id,
     title: parent => parent.title,
@@ -73,7 +18,7 @@ const resolvers = {
 }
 
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: './schema.graphql',
   resolvers
 })
 
