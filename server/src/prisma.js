6777d735352b08c009b223278a8b8896cf098caa
Corey Burns <coreymburns@gmail.com>
2020-02-29 07:50:20 -0500

    added create user/post/comment + update/ etc async

--- server/src/prisma.js
+++
@@ -1,8 +0,0 @@
-import { Prisma } from 'prisma-binding'
-
-const prisma = new Prisma({
-  typeDefs: 'src/generated/prisma.graphql',
-  endpoint: 'localhost:4466'
-})
-
-prisma.query.users(null, '{ id name email }')
--- server/src/resolvers/Mutation.js
+++ server/src/resolvers/Mutation.js
@@ -2,17 +2,46 @@
 // const bcrypt = require('bcryptjs')
 
 const Mutation = {
-  createUser: (root, { data }, { db }) => db.createUser({ ...data }),
-  createPost: (root, { data }, { db }) => db.createPost({ ...data }),
-  createComment: (root, { data }, { db }) => db.createComment({ ...data }),
+  createUser: async (root, { data }, { db }) =>
+    await db.createUser({ ...data }),
+  createPost: async (root, { data }, { db }) =>
+    await db.createPost({ ...data }),
+  createComment: async (root, { data }, { db }) =>
+    await db.createComment({ ...data }),
 
-  updateUser: (root, args, { db }) => {},
-  updatePost: (root, args, { db }) => {},
-  updateComment: (root, args, { db }) => {},
+  updateUser: async (root, args, { db }) =>
+    await db.updateUser({
+      data: {
+        role: 'ADMIN'
+      },
+      where: {
+        id: args.id
+      }
+    }),
+  updatePost: async (root, args, { db }) =>
+    await db.updatePost({
+      data: {
+        title: args.title
+      },
+      where: {
+        id: args.id
+      }
+    }),
+  updateComment: async (root, args, { db }) =>
+    await db.updateComment({
+      data: {
+        title: args.title
+      },
+      where: {
+        id: args.id
+      }
+    }),
 
-  deleteUser: (root, args, { db }) => {},
-  deletePost: (root, args, { db }) => {},
-  deleteComment: (root, args, { db }) => {}
+  deleteUser: async (root, { email }, { db }) =>
+    await db.deleteUser({ email: email }),
+  deletePost: async (root, { id }, { db }) => await db.deletePost({ id: id }),
+  deleteComment: async (root, { id }, { db }) =>
+    await db.deleteComment({ id: id })
 }
 
 export { Mutation as default }
