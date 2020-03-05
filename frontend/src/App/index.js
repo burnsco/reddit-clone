8ad8d7014029dcfa84a1d983071ffd26c7bb5a0b
Corey Burns <coreymburns@gmail.com>
2020-03-05 14:05:38 -0500

    animations, icons for auth setup, some other small changes

--- frontend/src/App/index.js
+++ frontend/src/App/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import Header from '../components/Header'
-import { AuthContext } from '../context/auth'
 import { Router } from '@reach/router'
 import { AppContainer } from './styles'
 import Home from '../pages/Home'
@@ -12,35 +11,30 @@ import AllPosts from '../components/PostList/AllPosts'
 import CreatePostPage from '../pages/CreatePost'
 import Comments from '../components/Comments'
 
-function UnauthenticatedApp() {
-  // Version without profile, shows sign up and login
-  // cant post, comment, etc
-}
+function UnauthenticatedApp() {}
 
-function AuthenticatedApp() {
-  // version with the profile, can sign out, post, comment
-}
+function AuthenticatedApp() {}
+
+function HomeApp() {}
 
 const App = () => (
   <AppContainer>
     <Header />
-    <AuthContext.Provider value={false}>
-      <Router>
-        <Profile path="profile/:userID">
-          <AllPosts path="profile/:userID/posts" />
-          <Comments path="profile/:userID/comments" />
-          <AllPosts path="/" />
-        </Profile>
-        <CreatePostPage path="submit" />
-        <LoginPage path="login" />
-        <Signup path="signup" />
-        <Home path="/">
-          <CategoryPosts path="r/:category" />
-          <Comments path="r/:category/:postID/comments" />
-          <AllPosts path="/" />
-        </Home>
-      </Router>
-    </AuthContext.Provider>
+    <Router>
+      <Profile path='profile/:userID'>
+        <AllPosts path='profile/:userID/posts' />
+        <Comments path='profile/:userID/comments' />
+        <AllPosts path='/' />
+      </Profile>
+      <CreatePostPage path='submit' />
+      <LoginPage path='login' />
+      <Signup path='signup' />
+      <Home path='/'>
+        <CategoryPosts path='r/:category' />
+        <Comments path='r/:category/:postID/comments' />
+        <AllPosts path='/' />
+      </Home>
+    </Router>
   </AppContainer>
 )
 
---
+++ frontend/src/components/Animations/index.js
@@ -0,0 +1,90 @@
+/** @jsx jsx */
+import { jsx } from '@emotion/core'
+
+import { keyframes } from '@emotion/core'
+import styled from '@emotion-styled'
+import * as colors from '../../styles/colors'
+import { Spinner3 } from '@styled-icons/evil'
+
+// SHARED THROUGHOUT THE APP
+
+// animation for the spinner
+const spin = keyframes({
+  '0%': { transform: 'rotate(0deg)' },
+  '100%': { transform: 'rotate(360deg)' }
+})
+
+// a system div (to use where you want throughout app)
+export const Centered = styled.div({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  width: '100%',
+  height: '100vh'
+})
+
+export const CircleButton = styled.button({
+  borderRadius: '30px',
+  padding: '0',
+  width: '40px',
+  height: '40px',
+  lineHeight: '1',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  background: colors.base,
+  color: colors.text,
+  border: `1px solid ${colors.gray10}`,
+  cursor: 'pointer'
+})
+
+export const BookListUL = styled.ul({
+  listStyle: 'none',
+  padding: '0',
+  display: 'grid',
+  gridTemplateRows: 'repeat(auto-fill, minmax(100px, 1fr))',
+  gridGap: '1em'
+})
+
+export function Spinner(props) {
+  return (
+    <Spinner3
+      css={{ animation: `${spin} 1s linear infinite` }}
+      aria-label='loading'
+      {...props}
+    />
+  )
+}
+
+const buttonVariants = {
+  primary: {
+    background: colors.indigo,
+    color: colors.base
+  },
+  secondary: {
+    background: colors.gray,
+    color: colors.text
+  }
+}
+export const Button = styled.button(
+  {
+    padding: '10px 15px',
+    border: '0',
+    lineHeight: '1'
+  },
+  ({ variant = 'primary' }) => buttonVariants[variant]
+)
+
+export const FormGroup = styled.div({
+  display: 'flex',
+  flexDirection: 'column'
+})
+
+export function FullPageSpinner() {
+  return (
+    <div css={{ marginTop: '3em', fontSize: '4em' }}>
+      <Spinner />
+    </div>
+  )
+}
