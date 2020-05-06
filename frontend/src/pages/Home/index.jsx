9490905ea844a9c0fa1a21f6ac4f6ead923f6e98
Corey Burns <coreymburns@gmail.com>
2020-05-06 08:23:08 -0400

    added a top control component for smaller screens, with create post and create category buttons as well!

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,14 +1,60 @@
 import React from 'react'
-import { FeedContainer, HomeContainer, SidebarContainer } from './styles.js'
+import {
+  FeedContainer,
+  HomeContainer,
+  SidebarContainer,
+  TopControls,
+  TopControlButtons,
+  TopControlSelect,
+  TopControlSelectContainer,
+} from './styles.js'
 import Categories from '../../components/Categories'
+import { useQuery } from '@apollo/client'
+import { GET_CATEGORIES } from '../CreatePost/query.js'
+import { ContainerHeader } from '../../components/Categories/styles.js'
+import MainSpinner from '../../components/shared/FallBackSpinner/index.js'
+import { useNavigate } from '@reach/router'
 
-const HomePage = ({ children }) => (
-  <HomeContainer>
-    <FeedContainer>{children}</FeedContainer>
-    <SidebarContainer>
-      <Categories />
-    </SidebarContainer>
-  </HomeContainer>
-)
+const HomePage = ({ children }) => {
+  const { loading, error, data } = useQuery(GET_CATEGORIES)
+  const navigate = useNavigate()
 
+  if (loading) return <MainSpinner />
+
+  if (error) {
+    console.log(error)
+    return <div>error, please return to where you came</div>
+  }
+
+  const handleSelect = selectedCategory => {
+    console.log(`Option selected: `, selectedCategory)
+    navigate(`/r/${selectedCategory.label}`, { replace: true })
+  }
+
+  const options = data.categories.map(option => {
+    return { value: option.id, label: option.name }
+  })
+  return (
+    <HomeContainer>
+      <FeedContainer>
+        <TopControls>
+          <TopControlSelectContainer>
+            <TopControlSelect
+              name="category"
+              options={options}
+              onChange={handleSelect}
+            />
+          </TopControlSelectContainer>
+          <TopControlButtons to="/submit">Post</TopControlButtons>
+          <TopControlButtons to="/createCategory">Category</TopControlButtons>
+        </TopControls>
+
+        {children}
+      </FeedContainer>
+      <SidebarContainer>
+        <Categories />
+      </SidebarContainer>
+    </HomeContainer>
+  )
+}
 export default HomePage
--- frontend/src/pages/Home/styles.js
+++ frontend/src/pages/Home/styles.js
@@ -1,7 +1,10 @@
 import styled from '@xstyled/styled-components'
+import { Link } from '@reach/router'
+import Select from 'react-select'
 
 export const HomeContainer = styled.div`
   display: flex;
+  min-width: 100%;
 `
 
 export const FeedContainer = styled.section`
@@ -15,5 +18,34 @@ export const SidebarContainer = styled.aside`
 
   @media (max-width: 768px) {
     display: none;
+  }
+`
+
+export const TopControls = styled.div`
+  display: flex;
+  width: 100%;
+  margin-bottom: 10px;
+  @media (min-width: 768px) {
+    display: none;
+  }
+`
+
+export const TopControlSelectContainer = styled.div`
+  flex-grow: 2;
+`
+export const TopControlSelect = styled(Select)``
+
+export const TopControlButtons = styled(Link)`
+  border: 1px solid white;
+  flex-grow: 1;
+  border-radius: 5px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #3661ed;
+  color: white;
+  border: none;
+  &:hover {
+    background-color: #8ca2ea;
   }
 `
