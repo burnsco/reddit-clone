4cd4d433405ac7aa0da957114e0e93fd5ae0dfdf
Corey Burns <coreymburns@gmail.com>
2020-05-25 11:28:43 -0400

    switched current user query

--- frontend/src/pages/Home/CreationButtons.js
+++ frontend/src/pages/Home/CreationButtons.js
@@ -2,9 +2,10 @@ import React from 'react'
 import { ContainerHeader } from './styles'
 import { useQuery } from '@apollo/client'
 import { CURRENT_USER } from '../Header/query'
+import { CURRENT_USER_QUERY } from '../../graphql/Query/current_user'
 
 const CreationButtons = () => {
-  const { loading, error, data } = useQuery(CURRENT_USER)
+  const { loading, error, data } = useQuery(CURRENT_USER_QUERY)
 
   if (data && data.currentUser) {
     return (
